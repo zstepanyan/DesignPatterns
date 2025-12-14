efc67ae525d10f1340e6fd85bdf1dbec87dd823d
zstepanyan <159805567+zstepanyan@users.noreply.github.com>
2025-12-14 21:42:46 +0400

    temp changes

--- regex/src/ast/ast.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../include/ast/ast.hpp"
-
-namespace ast {
-    node::node(node_type_t type, char value, bool negated) : type_(type), value_(value), negated_(negated) {}
-
-    node::node(node_type_t type, const std::set<char>& chars, bool negated) : type_(type), chars_(chars), negated_(negated) {}
-} // namespace ast
--- regex/src/match/match.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../include/match/match.hpp"
-
-namespace match {
-    match::match(bool matched, size_t start, size_t end) : matched_(matched), start_(start), end_(end) {}
-} // namespace match
